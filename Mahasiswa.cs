7fa83ee4f7cd5a1c7308e9bdd6e5f728273f8604
Mulana noorsy <maulana.noorsy22@gmail.com>
2019-03-19 18:39:31 +0700

    Class

---
+++ Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclassdanobject
+{
+    class mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("Nim : {0}", Nim);
+            Console.WriteLine("IPK : {0}", Ipk);
+            Console.WriteLine("\nTelsh melskuksn registrasi");
+
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses krs", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanclassdanobject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mahasiswa saya = new mahasiswa();
+            mahasiswa kamu = new mahasiswa();
+
+            saya.Nim = "19.11.2272";
+            saya.Nama = "jono";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "19.11.2276";
+            kamu.Nama = "Paijo";
+            kamu.Ipk = 3.70f;
+
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
